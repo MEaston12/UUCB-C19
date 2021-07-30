1e59b3ded279e9a611070452de6f73239d041af1
MEaston12 <eastonmalcolm@gmail.com>
2021-07-29 22:47:21 -0600

    added activate and fetch

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -11,3 +11,19 @@ self.addEventListener('install', e => {
     }
     e.waitUntil(preCache());
 });
+
+self.addEventListener('activate', e => {
+    const activate = async () => {
+        const keyList = await caches.keys();
+        return Promise.all(
+            keyList.filter(key => key.startsWith(APP_PREFIX) && key != CACHE_NAME)
+                .map(key => caches.delete(key))
+        );
+    }
+    e.waitUntil(activate());
+});
+
+self.addEventListener('fetch', e => {
+    console.log('got fetch:', e.request.url);
+    e.respondWith(caches.match(e.request).then(request => request || fetch(e.request)));
+});
